3900907ab7b3c97230c1d967e8fc83236b2d033f
internzone00 <internzone01@gmail.com>
2025-07-31 19:02:24 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Hero = () => {
+    const navigate = useNavigate();
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    
+
+    const handleInternalLink = (section) => {
+    setIsMobileMenuOpen(false); // Close mobile menu when a link is clicked
+    if (window.location.pathname === '/') {
+      const element = document.getElementById(section);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    } else {
+      navigate('/', { state: { scrollTo: section } });
+    }
+  };
+
     return (
         <section className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 px-6 py-12 md:py-20 items-center">
             <div className="space-y-6">
@@ -11,11 +28,11 @@ const Hero = () => {
                     InternZone is a student-focused platform providing real-world experience through internships and projects. Begin your journey to success today!
                 </p>
                 <div className="pt-2">
-                    <a href="#services">
-                        <button className="px-8 py-3 text-lg font-semibold rounded-full bg-cyan-400 text-white hover:bg-cyan-600 transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-cyan-500/30">
+                        <button
+                        onClick={() => handleInternalLink('services')}
+                        className="px-8 py-3 text-lg font-semibold rounded-full bg-cyan-400 text-white hover:bg-cyan-600 transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-cyan-500/30">
                             Explore Now →
                         </button>
-                    </a>
                 </div>
             </div>
             <div className="flex justify-center md:justify-end">
